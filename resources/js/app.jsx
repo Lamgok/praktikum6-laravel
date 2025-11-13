95d362322d516d4286eefa4c04caff9d3838dfb6
Lamgok <andosiahaan13@gmail.com>
2025-11-13 14:45:09 +0700

    psuh

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,15 +2,22 @@ import "../css/app.css";
 import "./bootstrap";
 
 // Import Trix (tidak perlu import JS karena sudah global)
-import 'trix/dist/trix.css';
+import "trix/dist/trix.css";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        return pages[`./Pages/${name}.jsx`];
+        const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+        // --- PERBAIKAN DIMULAI DI SINI ---
+        // Dapatkan objek modul
+        const pageModule = pages[`./Pages/${name}.jsx`];
+
+        // Cek dan kembalikan default export.
+        // Jika pageModule adalah undefined (halaman tidak ada), maka akan mengembalikan undefined.
+        return pageModule?.default;
+        // --- PERBAIKAN SELESAI DI SINI ---
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
